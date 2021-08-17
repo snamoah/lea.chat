73e5ee1b58e8d3b27dec4e0964bf4e77117e0842
Samuel Amoah <sa.am@programmer.net>
2021-08-17 17:49:40 +0200

    initialize preact app

---
+++ widget/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['preact', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['build/'],
+}
---
+++ widget/src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionalComponent, h } from 'preact'
+
+import Home from './pages/home'
+
+const App: FunctionalComponent = () => {
+  return (
+    <div id="preact_root">
+      <Home />
+    </div>
+  )
+}
+
+export default App

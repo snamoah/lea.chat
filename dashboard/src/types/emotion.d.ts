9da7cbccdd37f03dfdcfa297abf657d44d8402ae
Samuel Amoah <sa.am@programmer.net>
2021-08-19 14:05:18 +0200

    feat(dashboard): setup @emotion in project

---
+++ dashboard/src/types/emotion.d.ts
@@ -0,0 +1,20 @@
+import '@emotion/react'
+
+interface ThemeVariants {
+  default: string
+  light: string
+  dark: string
+  darker: string
+}
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: {
+      blue: ThemeVariants
+      red: ThemeVariants
+      yellow: ThemeVariants
+      green: ThemeVariants
+      grey: ThemeVariants
+    }
+  }
+}
---
+++ dashboard/src/ui/theme.ts
@@ -0,0 +1,36 @@
+const theme = {
+  colors: {
+    blue: {
+      default: '#0E7BE4',
+      light: '#388DE0',
+      dark: '#0466C8',
+      darker: '#023B73',
+    },
+    red: {
+      default: '#FF5300',
+      light: '#FF7330',
+      dark: '#E54A00',
+      darker: '#B13900',
+    },
+    yellow: {
+      default: '#FFC300',
+      light: '#FFCE30',
+      dark: '#E5AF00',
+      darker: '#B18800',
+    },
+    green: {
+      default: '#00CB83',
+      light: '#2AE2A1',
+      dark: '#009963',
+      darker: '#00774D',
+    },
+    grey: {
+      default: '#7D8597',
+      light: '#979DAC',
+      dark: '#5C677D',
+      darker: '#33415C',
+    },
+  },
+}
+
+export default theme
